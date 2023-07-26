632518dbad43a7d73954ef740951d2b12c9d6681
minkycho0412 <minkycho0412@gmail.com>
2023-07-26 23:40:17 +0900

    FEAT: 장고 통신 수정중

--- ai_django/pet/views.py
+++ ai_django/pet/views.py
@@ -1,18 +1,22 @@
+from django.core.files.storage import FileSystemStorage
 from django.http import JsonResponse
 import subprocess
 import json
 import os
-from django.core.files.storage import FileSystemStorage
+import requests
 
 def result(request):
+    file = None
+    access_token = None
+    
     if request.method == "POST" and request.FILES.get("file"):
         file = request.FILES["file"]
-        token = request.POST.get("token")
+        pet_id = request.POST.get("pet_id")
+        access_token = request.POST.get("token")
         
-        # fs = FileSystemStorage('/')
-        # filename = fs.save(file.name, file)
-        # file_url = fs.url(filename)
-    
+        fs = FileSystemStorage()
+        fs.save('demo3.png', file)
+        
     activate_cmd = f"conda activate copet"
     cd_cmd = "cd for_inference"
     
@@ -25,18 +29,27 @@ def result(request):
     data = json.load(f)
     labels = data.get('labels')
     scores = data.get('scores')
-    print(print_output(labels, scores))
-
-
-    # headers = {'Content-Type': 'application/json'}
-    # url = 'http://localhost:8080/pet/result'
-    # json_data = json.dumps(data)  # 딕셔너리를 JSON 문자열로 변환
-    # response = requests.post(url, data=payload)
     
-    # 응답의 JSON 데이터를 파싱하여 반환
+    files = {'file': file}
+    data = {
+        'pet_id': pet_id,
+        'disase_name': output(labels, scores)
+    }
+
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'multipart/form-data'
+    }
+    url = 'http://localhost:8080/pet/result'
+    # response = requests.post(url, files= files, data=data, headers=headers)
+    
+    # # 응답의 JSON 데이터를 파싱하여 반환
     # response_data = response.json()
     
-    return JsonResponse({'message': '요청이 성공적으로 전송되었습니다.'})
+    # return JsonResponse({'result': response_data})
+    
+    return JsonResponse({'result': output(labels, scores)})
+
     # 응답 확인
     # if response.status_code == 200:
     #     return JsonResponse({'message': '요청이 성공적으로 전송되었습니다.'})
@@ -44,10 +57,20 @@ def result(request):
     #     return JsonResponse({'message': '요청 전송에 실패했습니다.', 'status_code': response.status_code})
 
 
-def print_output(labels, scores):
+def output(labels, scores):
 
     category_names = ["증상없음", "각막궤양", "각막부골편", "결막염", "비궤양성각막염", "안검염"]
 
     max_idx = scores.index(max(scores))
 
     return category_names[labels[max_idx]]
+
+
+def read_file():
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_directory, 'test.png')
+
+    with open(file_path, 'rb') as file:
+        file_content = file.read()
+
+    return file_content
